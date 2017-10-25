eb89484a3505fa200dbb0f784022b47bfdcb56cc
oakes <zach@sekao.net>
2017-10-24 23:21:34 -0400

    Fix building cljs from jar file

--- src/clj/lightmod/game.clj
+++ src/clj/lightmod/game.clj
@@ -6,20 +6,30 @@
             [ring.util.response :refer [redirect not-found]]
             [ring.util.request :refer [body-string]]
             [nightcode.state :refer [runtime-state]]
-            [cljs.build.api :refer [build]]))
+            [cljs.build.api :refer [build]]
+            [nightcode.utils :as u]))
+
+(def redirects {"/" "/index.html"
+                "/main.js" "/.out/main.js"})
+
+(def ending-redirects {"/p5.js" "/.out/js/p5.js"
+                       "/p5.tiledmap.js" "/.out/js/p5.tiledmap.js"})
 
 (defn handler [request]
-  (or (case (:uri request)
-        "/" (redirect "/index.html")
-        "/main.js" (redirect ".out/main.js")
-        nil)
-      (let [uri (:uri request)
-            uri (if (.startsWith uri "/") (subs uri 1) uri)
-            file (io/file (:project-dir @runtime-state) uri)]
-        (when (.exists file)
-          {:status 200
-           :body (slurp file)}))
-      (not-found "")))
+  (let [uri (:uri request)]
+    (or (when-let [new-uri (redirects uri)]
+          (redirect new-uri))
+        (when-let [new-uri (some (fn [[k v]]
+                                   (when (.endsWith uri k) v))
+                             ending-redirects)]
+          (when (not= uri new-uri)
+            (redirect new-uri)))
+        (let [uri (if (.startsWith uri "/") (subs uri 1) uri)
+              file (io/file (:project-dir @runtime-state) uri)]
+          (when (.exists file)
+            {:status 200
+             :body (slurp file)}))
+        (not-found ""))))
 
 (defn start-web-server! []
   (-> handler
@@ -35,7 +45,17 @@
       {:output-to (.getCanonicalPath (io/file dir ".out" "main.js"))
        :output-dir (.getCanonicalPath (io/file dir ".out"))
        :main (str (.getName (io/file dir)) ".core")
-       :asset-path ".out"}))
+       :asset-path ".out"
+       ; apparently :foreign-libs requires the paths to be relative
+       :foreign-libs [{:file (u/get-relative-path
+                               (.getCanonicalPath (io/file "."))
+                               (.getCanonicalPath (io/file dir ".out" "js" "p5.js")))
+                       :provides ["p5.core"]}
+                      {:file (u/get-relative-path
+                               (.getCanonicalPath (io/file "."))
+                               (.getCanonicalPath (io/file dir ".out" "js" "p5.tiledmap.js")))
+                       :provides ["p5.tiled-map"]
+                       :requires ["p5.core"]}]}))
   (let [port (start-web-server!)
         game (.lookup scene "#game")
         engine (.getEngine game)]

7b11155a7b5a5bf3220860873e58e0636c00d1bd
oakes <zach@sekao.net>
2018-01-06 11:48:56 -0500

    Remove MAS files

--- package-app-store.sh
+++
@@ -1,1 +0,0 @@
-javapackager -deploy --add-modules java.xml.bind -native mac.appStore -outdir package -outfile Lightmod -srcdir target -appclass lightmod.core -name "Lightmod" -title "Lightmod" -Bicon=package/macosx/Lightmod.icns -BappVersion=$1 -Bmac.app-store-entitlements=package/macosx/Lightmod.entitlements -Bmac.CFBundleIdentifier=net.sekao.lightmod

7d12f3785c5b925ac283efb74fc6217a44e78919
oakes <zach@sekao.net>
2019-01-21 21:19:38 -0600

    Increment version number

--- src/clj/lightmod/core.clj
+++ src/clj/lightmod/core.clj
@@ -24,7 +24,7 @@
            [javafx.beans.value ChangeListener])
   (:gen-class :extends javafx.application.Application))
 
-(def version "1.2.1")
+(def version "1.3.0")
 
 (def actions {:#up c/up!
               :#save c/save!

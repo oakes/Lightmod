b143cf8df09baeff6e6bf48c6cc64890855aa762
oakes <zach@sekao.net>
2019-09-21 19:18:44 -0400

    Start 1.4.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lightmod "1.4.0"
+(defproject lightmod "1.4.1-SNAPSHOT"
   :description "An all-in-one tool for full stack Clojure"
   :url "https://github.com/oakes/Lightmod"
   :license {:name "Public Domain"

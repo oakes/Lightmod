28728bdccdb650f8fc21cbf8cdd954fcca6d58ef
oakes <zach@sekao.net>
2019-12-22 14:03:02 -0500

    Update windows package script

--- package-win.bat
+++ package-win.bat
@@ -1,1 +1,3 @@
+jpackage --type app-image --name Lightmod --app-version %1 --input target --main-jar Lightmod-%1-windows.jar --icon package/windows/Lightmod.ico --copyright "Public Domain"
+
 jpackage --type exe --name Lightmod --app-version %1 --input target --main-jar Lightmod-%1-windows.jar --icon package/windows/Lightmod.ico --copyright "Public Domain" --win-menu --win-per-user-install
